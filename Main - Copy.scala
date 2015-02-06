7e72fa565e6d8c1f5a3954756e7657b6e7a306b5
Akash Shah <akash1987@gmail.com>
2015-02-06 11:29:26 -0600

    2nd Sample

---
+++ Main - Copy.scala
@@ -0,0 +1,57 @@
+import java.util.Random
+
+object HelloWorld {
+
+  def main(args: Array[String]) {
+
+    val source = scala.io.Source.fromFile("quotes.txt", "UTF-8")
+    val quotes = source.mkString.split("\n")
+
+    val quotesList = quotesIntoList(quotes)
+    val randomizedQuotes = randomizeQuotes(quotesList)
+
+    randomizedQuotes.foreach(println)
+
+    println("Hello, world!")
+  }
+
+  def quotesIntoList(quotes: Array[String]): List[String] = {
+
+    var str = ""
+    var quotesList = List[String]()
+
+    for (i <- 0 until quotes.length) {
+
+      if (quotes(i).trim.length > 0) {
+        str = str + s"""${quotes(i)}""" + "\n"
+      } else {
+        quotesList = str.trim :: quotesList
+        str = ""
+      }
+    }
+
+    quotesList
+  }
+
+  def randomizeQuotes(quotes: List[String]): List[String] = {
+
+    val t = quotes
+    val t2 = t.zipWithIndex
+    var tempQuotes = quotes
+    var newQuotes = List[String]()
+
+    val random = new Random(System.currentTimeMillis())
+    var randomInt = 0
+
+    for (i <- quotes.length to 1 by -1) {
+      randomInt = random.nextInt(tempQuotes.length); println(tempQuotes.length)
+      newQuotes = tempQuotes(randomInt) :: newQuotes
+      tempQuotes = tempQuotes.drop(randomInt)
+    }
+
+    println(quotes.size)
+    println(newQuotes.size)
+
+    newQuotes
+  }
+}
